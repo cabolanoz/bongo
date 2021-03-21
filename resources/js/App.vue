219d6b78042b4793b178c42c0861cb67af45e20b
César Bolaños <cesar.bolanos.andino@gmail.com>
2021-03-20 23:20:35 -0600

    Create basic navigation component

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+        <SocialStickGroup></SocialStickGroup>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import Navbar from './Components/Navbar.vue';
+import SocialStickGroup from './Components/SocialStickGroup.vue';
+
+export default {
+    name: 'app',
+    components: { Navbar, SocialStickGroup }
+}
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import BootstrapVue from 'bootstrap-vue'
+
+import router from './Router/index';
+import store from './Store/index';
+import App from './App.vue';
+
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App }
+});
